18940b10daf9573a58ef475d1d700f71d705f29d
Kwame Dohgbefu <kwamedohgbefu@outlook.com>
2022-11-06 12:20:07 +0000

    adding basis

---
+++ script.js
@@ -0,0 +1,9 @@
+function calc(a, b) {
+    let calc = a + b;
+    if (calc > 2) {
+        return ('the people are more than two');
+    }else{
+        return ('either no one registered ')
+    }
+
+} 
