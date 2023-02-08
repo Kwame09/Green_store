38886e5769203c27b0be74f0281aa4aac4f4d97d
Kwame09 <kwamedohgbefu@outlook.com>
2023-02-07 15:17:41 -1100

    working on the homa page

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-function calc(a, b) {
-    let calc = a + b;
-    if (calc > 2) {
-        return ('the people are more than two');
-    }else{
-        return ('either no one registered ')
-    }
+const dropdownFunction = ()=>{
+    const dropdowns = document.querySelectorAll('.dropdown')
+    dropdowns.forEach((elem, index)=>{
+        elem.addEventListener('mouseenter', e => {
+            const icon = elem.querySelector('.dropdown-icon')
 
-} 
+            setTimeout(()=>{
+                icon.classList.remove('fa-caret-down')
+                icon.classList.add('fa-caret-up')
+            },100)
+        })
+
+        elem.addEventListener('mouseleave', e => {
+            const icon = elem.querySelector('.dropdown-icon')
+            
+            setTimeout(()=>{
+                icon.classList.remove('fa-caret-up')
+                icon.classList.add('fa-caret-down')
+            },100)
+        })
+    })
+
+    const dropdownItems = document.querySelectorAll('.dropdown-item')
+
+    dropdownItems.forEach((elem, index)=>{
+        elem.addEventListener('click', e => {
+            const currentDropdown = e.target.parentElement.parentElement
+            const currentDropdownItem = currentDropdown.querySelector('.dropdown-current')
+            currentDropdownItem.textContent = e.target.innerText
+        })
+    })
+}
+
+window.onload = (e)=>{
+    dropdownFunction()
+}
